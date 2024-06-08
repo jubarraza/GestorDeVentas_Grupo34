c96d00b272b068017dd11a6c18bbd388cbb1ae2c
Julieta <jubarraza5@gmail.com>
2024-06-08 20:51:26 -0300

    update Persona
    Se cambia el int de dni por un long (no se resuelve el bug :-1: )

--- Persona.cpp
+++ Persona.cpp
@@ -7,13 +7,13 @@ using namespace std;
 //Constructores
 
 Persona::Persona() {
-    _dni = 0;
-    strcpy(_nombre, "");
-    strcpy(_apellido, "");
-    _fechaNacimiento = Fecha();
+    setDni(1000000);
+    strcpy(_nombre, " ");
+    strcpy(_apellido, " ");
+    setFechaNacimiento(Fecha());
 }
 
-Persona::Persona(int dni, std::string nombre, std::string apellido, Fecha fechaNacimiento) {
+Persona::Persona(long dni, std::string nombre, std::string apellido, Fecha fechaNacimiento) {
     setDni(dni);
     setNombre(nombre);
     setApellido(apellido);
@@ -22,7 +22,7 @@ Persona::Persona(int dni, std::string nombre, std::string apellido, Fecha fechaN
 
 //Getters
 
-int Persona::getDni() {
+long Persona::getDni() {
     return _dni;
 }
 
@@ -47,7 +47,7 @@ Fecha Persona::getFechaNacimiento() {
 
 //Setters
 
-void Persona::setDni(int dni) {
+void Persona::setDni(long dni) {
     if (dni > 999999) {
         _dni = dni;
     }
@@ -60,7 +60,7 @@ void Persona::setDni(int dni) {
 }
 
 void Persona::setNombre(std::string nombre) {
-    if (nombre.size() <= 40) { 
+    if (nombre.size() <= 39) { 
         strcpy(_nombre, nombre.c_str()); 
     }
     else {
@@ -71,7 +71,7 @@ void Persona::setNombre(std::string nombre) {
 }
 
 void Persona::setApellido(std::string apellido) {
-    if (apellido.size() <= 40) {
+    if (apellido.size() <= 39) {
         strcpy(_apellido, apellido.c_str()); 
     }
     else {
@@ -84,32 +84,4 @@ void Persona::setApellido(std::string apellido) {
 
 void Persona::setFechaNacimiento(Fecha fechaNacimiento) {
     _fechaNacimiento = fechaNacimiento;
-}
-
-void Persona::Cargar() {
-    int dni;
-    string nombre, apellido; 
-    Fecha f;
-
-    cout << "DNI: ";
-    cin >> dni;
-    //cin.ignore();
-    setDni(dni);
-    cin.ignore();
-    cout << "NOMBRE: ";
-    getline(cin, nombre);
-    setNombre(nombre);
-    cout << "APELLIDO: ";
-    getline(cin, apellido);
-    setApellido(apellido);
-    cout << "FECHA NACIMIENTO: " << endl;
-    _fechaNacimiento.Cargar();
-}
-
-void Persona::Mostrar() {
-    cout << "DNI: " << _dni << std::endl;
-    cout << "NOMBRE: " << _nombre << std::endl;
-    cout << "APELLIDO: " << _apellido << std::endl;
-    cout << "FECHA NACIMIENTO: ";
-    cout << _fechaNacimiento.toString() << std::endl;
 }
--- Persona.h
+++ Persona.h
@@ -4,27 +4,24 @@
 class Persona
 {
 protected:
-        int _dni;
+        long _dni;
         char _nombre[40];
         char _apellido[40];
         Fecha _fechaNacimiento;
 public:
         Persona();
-        Persona(int dni, std::string nombre, std::string apellido, Fecha fechaNacimiento);
+        Persona(long dni, std::string nombre, std::string apellido, Fecha fechaNacimiento);
 
-        int getDni();
+        long getDni();
         std::string getNombre();
         std::string getApellido();
         std::string getApellidoNombre();
         Fecha getFechaNacimiento();
 
-        void setDni(int dni); //tiene validacion para que el nro siempre sea de almenos 7 digitos
+        void setDni(long dni); //tiene validacion para que el nro siempre sea de almenos 7 digitos
         void setNombre(std::string nombre); //tiene validacion de numero de caracteres 
         void setApellido(std::string apellido); //tiene validacion de numero de caracteres 
         void setFechaNacimiento(Fecha fechaNacimiento);
-
-        void Cargar();
-        void Mostrar();
 
 };
 
