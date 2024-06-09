5b0751c8d90d791ecf4052dbb48006def0140c0b
Julieta <jubarraza5@gmail.com>
2024-06-09 11:42:58 -0300

    update main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
 #include <cstring>
+#include "Direccion.h"
+#include "Sucursal.h"
 #include "VentasManager.h"
+#include "VendedorManager.h"
+#include "ClienteManager.h"
+#include "VehiculosManager.h"
+#include "SucursalManager.h"
+#include "Informes.h"
+
 using namespace std;
 
 
 
 int main()
 {
-    VentasManager vm(VentasArchivo("Ventas.dat"));
+    setlocale(LC_ALL, "Spanish_Spain.1252");
+    VentasManager ventam;
+    VendedorManager vendedorm;
+    ClienteManager clientem;
+    VehiculosManager vehiculom;
+    SucursalManager sucursalm; 
+    Informes inf;
 
-    vm.menu();
+    //inf.Menu();
+
+    vehiculom.Menu();
+    //clientem.menu();
+    //ventam.menu();
+    //vendedorm.Menu();
+    //sucursalm.menu();
 
     return 0;
 }
