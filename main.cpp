f0736788a8d54b36f3eda6aab87159bfb3e656fc
Julieta <jubarraza5@gmail.com>
2024-06-11 20:08:29 -0300

    main armado con clase Sistema

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS 
+﻿#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
-#include <cstring>
-#include "Direccion.h"
-#include "Sucursal.h"
-#include "VentasManager.h"
-#include "VendedorManager.h"
-#include "ClienteManager.h"
-#include "VehiculosManager.h"
-#include "SucursalManager.h"
-#include "Informes.h"
+#include "Sistema.h"
 
 using namespace std;
 
@@ -17,20 +9,9 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Spanish_Spain.1252");
-    VentasManager ventam;
-    VendedorManager vendedorm;
-    ClienteManager clientem;
-    VehiculosManager vehiculom;
-    SucursalManager sucursalm; 
-    Informes inf;
+    Sistema programa;
 
-    //inf.Menu();
-
-    //vehiculom.Menu();
-    //clientem.Menu();
-    ventam.Menu();
-    //vendedorm.Menu();
-    //sucursalm.Menu();
+    programa.Menu();
 
     return 0;
 }
