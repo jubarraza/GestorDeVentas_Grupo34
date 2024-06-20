9f178d728ec20ed154b0bad257bfe2ebbbeecd8c
Julieta <jubarraza5@gmail.com>
2024-06-20 18:47:58 -0300

    se agrega el menu Integrantes

--- Sistema.cpp
+++ Sistema.cpp
@@ -24,9 +24,13 @@ void Sistema::Menu()
         cout << " 6) Informes " << endl;
         cout << " 7) Configuracion " << endl;
         cout << endl;
+        cout << "---------------------------" << endl;
+        cout << " 8) Integrantes" << endl;
+        cout << "---------------------------" << endl;
+        cout << endl;
         cout << " 0) Salir del Programa" << endl;
         cout << "---------------------------" << endl;
-        cout << "- Selecione una opcion: ";
+        cout << "Selecione una opcion: ";
         cin >> opc;
         system("cls");
         switch (opc) {
@@ -44,6 +48,9 @@ void Sistema::Menu()
             break;
         case 7: _configuracion.Menu();
             break;
+        case 8: Creditos();
+            system("pause");
+            break;
         case 0:cout << endl << " Adios!! " << endl;
             break;
         default:cout << endl << "* Selecione una Opcion Correcta! *" << endl << endl;;
@@ -51,4 +58,35 @@ void Sistema::Menu()
         }
 
     } while (opc != 0);
+}
+
+void Sistema::Creditos()
+{
+    cout << "------------------ GRUPO 34: ------------------" << endl;
+    cout << endl;
+    cout << "-----------------------------------------------" << endl;
+    cout << "		Integrantes:				" << endl;
+    cout << "-----------------------------------------------" << endl;
+    cout << endl;
+    cout << "1) Julieta Barraza" << endl;
+    cout << "2) Jonatan Guzman" << endl;
+    cout << "3) Yamil Diaz " << endl;
+    cout << "4) Jesica Valdez";
+    cout << endl << endl;
+    cout << "-----------------------------------------------" << endl;
+    cout << "		Profesor asignado:   		" << endl;
+    cout << "-----------------------------------------------" << endl;
+    cout << endl;
+    cout << " Mariano Varela								";
+    cout << endl << endl;
+    cout << "-----------------------------------------------" << endl;
+    cout << "		Universidad:				" << endl;
+    cout << "-----------------------------------------------" << endl;
+    cout << "Universidad Tecnologica Nacional				" << endl;
+    cout << "Facultad Regional General Pacheco				" << endl;
+    cout << "Tecnicatura Universitaria en Programacion TUP	" << endl;
+    cout << "Laboratorio de Programacion II" << endl << endl;
+    cout << "-----------------------------------------------" << endl;
+    cout << "		Año 2024					" << endl;
+    cout << "-----------------------------------------------" << endl;
 }
--- Sistema.h
+++ Sistema.h
@@ -25,5 +25,6 @@ private:
 public:
 	Sistema();
 	void Menu();
+	void Creditos();
 };
 
