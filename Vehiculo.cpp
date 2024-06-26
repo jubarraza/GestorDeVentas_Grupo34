b5236c9a3e37da060107bc55eccebb24b2e527d4
Julieta <jubarraza5@gmail.com>
2024-06-26 17:43:45 -0300

    orden include

--- Vehiculo.cpp
+++ Vehiculo.cpp
@@ -1,9 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
-using namespace std;
 #include "Vehiculo.h"
 #include "Fecha.h"
+using namespace std;
 
 Vehiculo::Vehiculo() {
     Fecha anio;
